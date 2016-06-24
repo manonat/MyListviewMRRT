96da41726447d693c30d09c9490013ca4ce80170
MRRT <buddhacomic@hotmail.com>
2016-06-24 10:41:10 +0700

    test github

---
+++ app/src/main/java/com/virtualsiam/mylistviewmrrt/MainActivity.java
@@ -0,0 +1,16 @@
+package com.virtualsiam.mylistviewmrrt;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
